7bd30631271867bcee0454205db19b870da75e40
icarusxo <icarusxo@gmail.com>
2022-04-11 18:58:27 +0900

    test

--- adm/application/controllers/Counselq.php
+++ adm/application/controllers/Counselq.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 require_once 'Login.php';
 
-class Joinq extends Login
+class Counselq extends Login
 {
 	public function __construct()
 	{
